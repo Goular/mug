7b9cffac4c04e5a66db49f169f65c1b561a86cf3
Goular <zhaojt_cs@126.com>
2018-07-12 14:39:46 +0800

    添加 babel-runtime,fastclick,babel-polyfill,jsonp 的npm库

---
+++ src/components/dataTraceability/data-traceability.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Hello World -222</div>
+</template>
+
+<script>
+export default {
+  name: 'data-traceability'
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// 为单个页面添加Title
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
