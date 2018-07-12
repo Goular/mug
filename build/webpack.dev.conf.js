ea0e3eaa2683833acf972b46e05ed6d3a03b3d5c
Goular <zhaojt_cs@126.com>
2018-07-12 17:09:11 +0800

    添加代理访问的代码

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -10,12 +10,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
+// ------------------- 2018-07-12 添加API代理访问 --------------------------
+const qs = require('qs')
+// 2018-01-13 添加代理访问的设置
+var axios = require('axios')
+// nodejs开发框架express，用来简化操作
+const express = require('express')
+//创建express应用程序
+const app = express()
+// 获取一个 express 的路由实例
+var apiRoutes = express.Router()
+// 添加路由的路径
+app.use('/api', apiRoutes)
+// -----------------------------------------------------------------------
+
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
 
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
-    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
+    rules: utils.styleLoaders({sourceMap: config.dev.cssSourceMap, usePostCSS: true})
   },
   // cheap-module-eval-source-map is faster for development
   devtool: config.dev.devtool,
@@ -25,7 +39,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
-        { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
+        {from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html')},
       ],
     },
     hot: true,
@@ -35,13 +49,24 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     port: PORT || config.dev.port,
     open: config.dev.autoOpenBrowser,
     overlay: config.dev.errorOverlay
-      ? { warnings: false, errors: true }
+      ? {warnings: false, errors: true}
       : false,
     publicPath: config.dev.assetsPublicPath,
     proxy: config.dev.proxyTable,
     quiet: true, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
+    },
+    before(app) {
+      app.get('/api/getChartData', (req, res) => {
+        let url = 'https://pcbapi.pcbbao.com/mesdemo/index.php/wx_jinpeng/token/trace'
+        let postData = qs.stringify({category_id: '1'})
+        axios.post(url, postData).then((response) => {
+          res.json(response.data)
+        }).catch((e) => {
+          console.log(e)
+        })
+      })
     }
   },
   plugins: [
@@ -85,8 +110,8 @@ module.exports = new Promise((resolve, reject) => {
           messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
         },
         onErrors: config.dev.notifyOnErrors
-        ? utils.createNotifierCallback()
-        : undefined
+          ? utils.createNotifierCallback()
+          : undefined
       }))
 
       resolve(devWebpackConfig)
--- src/api/testChart.js
+++ src/api/testChart.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // 获取测试图表数据
 export function getChartData () {
-  let url = 'https://pcbapi.pcbbao.com/mesdemo/index.php/wx_jinpeng/token/trace'
+  let url = 'http://localhost:8081/api/getChartData'
   const data = Object.assign({})
   return axios.get(url, {params: data}).then((res) => {
     return Promise.resolve(res.data)
