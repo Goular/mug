ea909f0e7b1d56c785310b3ba1e94c1a38149b36
Goular <zhaojt_cs@126.com>
2018-07-13 17:19:46 +0800

    提交内容

--- src/api/testChart.js
+++ src/api/testChart.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // 获取测试图表数据
 export function getChartData () {
-  let url = 'http://localhost:8081/api/getChartData'
+  let url = 'http://localhost:8080/api/getChartData'
   const data = Object.assign({})
   return axios.get(url, {params: data}).then((res) => {
     return Promise.resolve(res.data)
