f5c24b53ea2470a4e9c40ffc1042a066c34cb2c3
Goular <zhaojt_cs@126.com>
2018-07-16 11:34:44 +0800

    添加日期点击事件

--- src/base/dateSelector/date-selector.vue
+++ src/base/dateSelector/date-selector.vue
@@ -1,16 +1,33 @@
 <template>
-  <div class="container">
-    <div class="button">
-      <span>本日</span>
-      <img src="./arrow_triangle_down.png"/>
+  <div >
+    <div class="container">
+      <div class="button" @click="switchSelector">
+        <span>本日</span>
+        <img src="./arrow_triangle_down.png"/>
+      </div>
+      <div class="blank"></div>
     </div>
-    <div class="blank"></div>
+    <!-- 显示可以选择的内容 -->
+    <div>
+      <div class="date-item" @click="selectDate">本日</div>
+      <div class="date-item" @click="selectDate">本周</div>
+      <div class="date-item" @click="selectDate">本月</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'date-selector'
+  name: 'date-selector',
+  methods: {
+    switchSelector (e) { // 开启或者关闭日期选择器
+      console.dir(e)
+    },
+    selectDate (e) { // 选择日期
+      console.log('1122334')
+      console.dir(e)
+    }
+  }
 }
 </script>
 
@@ -33,4 +50,14 @@ export default {
         margin-left 24px
     .blank
       flex 1
+  .date-item
+    background-color #011128
+    font-size 32px
+    color #ff8a55
+    width 176px
+    padding-top 16px
+    padding-bottom 16px
+    text-align center
+    align-content center
+    margin-top 1px
 </style>
