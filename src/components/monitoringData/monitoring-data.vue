40f0388488ee8d45f461e409c0c45ce676b311d9
goular <zhaojt_cs@126.com>
2018-07-19 23:26:11 +0800

    提交信息

--- src/components/monitoringData/monitoring-data.vue
+++ src/components/monitoringData/monitoring-data.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="contianer">
+  <div class='contianer'>
     <date-selector></date-selector>
-    <div class="main">
-      <!--<div class="chartPanel">-->
+    <div v-if="revData" class='main'>
+      <!--<div class='chartPanel'>-->
         <!-- 图表区域 -->
-        <ve-line class="chart" :height="chart1Height" :data="chartData1" :extend="extend1"></ve-line>
-        <ve-line class="chart m-top-12" :height="chart1Height" :data="chartData1" :extend="extend2"></ve-line>
-      <ve-line class="chart m-top-12" :height="chart1Height" :data="chartData1" :extend="extend3"></ve-line>
-        <div class="m-top-12"></div>
+        <ve-line class='chart' :height='chart1Height' :data='revData.chartArr[0]' :extend='revData.extendArr[0]'></ve-line>
+        <ve-line class='chart m-top-12' :height='chart1Height' :data='revData.chartArr[1]' :extend='revData.extendArr[1]'></ve-line>
+        <ve-line class='chart m-top-12' :height='chart1Height' :data='revData.chartArr[2]' :extend='revData.extendArr[2]'></ve-line>
+        <div class='m-top-12'></div>
       <!--</div>-->
     </div>
   </div>
@@ -15,32 +15,30 @@
 
 <script>
 import DateSelector from 'base/dateSelector/date-selector'
+import {getMonitorData} from '../../api/monitor'
 
 export default {
   components: {DateSelector},
   name: 'monitoring-data',
   data: function () {
     return {
+      revData: null,
       chart1Height: '300px',
-      char1Width: '370px',
-      chartData1: {
-        columns: ['日期', '访问用户', '下单用户', '下单率'],
-        rows: [
-          { '日期': '1/1', '访问用户': 1393, '下单用户': 1093, '下单率': 0.32 },
-          { '日期': '1/2', '访问用户': 3530, '下单用户': 3230, '下单率': 0.26 },
-          { '日期': '1/3', '访问用户': 2923, '下单用户': 2623, '下单率': 0.76 },
-          { '日期': '1/4', '访问用户': 1723, '下单用户': 1423, '下单率': 0.49 },
-          { '日期': '1/5', '访问用户': 3792, '下单用户': 3492, '下单率': 0.323 },
-          { '日期': '1/6', '访问用户': 4593, '下单用户': 4293, '下单率': 0.78 },
-          { '日期': '1/7', '访问用户': 1393, '下单用户': 1093, '下单率': 0.32 },
-          { '日期': '1/8', '访问用户': 3530, '下单用户': 3230, '下单率': 0.26 },
-          { '日期': '1/9', '访问用户': 2923, '下单用户': 2623, '下单率': 0.76 },
-          { '日期': '1/10', '访问用户': 1723, '下单用户': 1423, '下单率': 0.49 },
-          { '日期': '1/11', '访问用户': 3792, '下单用户': 3492, '下单率': 0.323 },
-          { '日期': '1/12', '访问用户': 4593, '下单用户': 4293, '下单率': 0.78 }
-        ]
-      },
-      extend1: {
+      char1Width: '370px'
+    }
+  },
+  methods: {
+    _getMonitorList () {
+      getMonitorData().then((res) => {
+        this.$nextTick(() => {
+          this.revData = this.dealWithRawData(res.data)
+          console.dir(res.data)
+        })
+      })
+    },
+    dealWithRawData (rawData) {
+      // 创建喷幅数据
+      let extendPenfu = {
         'xAxis.0.axisLabel.rotate': 45,
         legend: {
           y: 'bottom',
@@ -49,51 +47,7 @@ export default {
         },
         title: {
           show: true,
-          text: '喷幅数据: 100片',
-          // subtext: '副标题',
-          padding: [16, 8, 8, 8],
-          left: 10,
-          textStyle: {
-            fontWeight: 'normal',
-            fontSize: 16
-          }
-        },
-        grid: {
-          bottom: 30
-        }
-      },
-      extend2: {
-        'xAxis.0.axisLabel.rotate': 45,
-        legend: {
-          y: 'bottom',
-          padding: [10, 10],
-          selectedMode: false // 取消图例上的点击事件
-        },
-        title: {
-          show: true,
-          text: '雾化数据: 102片',
-          // subtext: '副标题',
-          padding: [16, 8, 8, 8],
-          left: 10,
-          textStyle: {
-            fontWeight: 'normal',
-            fontSize: 16
-          }
-        },
-        grid: {
-          bottom: 30
-        }
-      },
-      extend3: {
-        'xAxis.0.axisLabel.rotate': 45,
-        legend: {
-          y: 'bottom',
-          padding: [10, 10],
-          selectedMode: false // 取消图例上的点击事件
-        },
-        title: {
-          show: true,
-          text: '油墨桶数据: 104片',
+          text: rawData.penfu_title + rawData.unit,
           // subtext: '副标题',
           padding: [16, 8, 8, 8],
           left: 10,
@@ -106,16 +60,99 @@ export default {
           bottom: 30
         }
       }
+      // 创建生产数量数据
+      let extendWuhua = {
+        'xAxis.0.axisLabel.rotate': 45,
+        legend: {
+          y: 'bottom',
+          padding: [10, 10],
+          selectedMode: false // 取消图例上的点击事件
+        },
+        title: {
+          show: true,
+          text: rawData.wuhua_title + rawData.unit,
+          // subtext: '副标题',
+          padding: [16, 8, 8, 8],
+          left: 10,
+          textStyle: {
+            fontWeight: 'normal',
+            fontSize: 16
+          }
+        },
+        grid: {
+          bottom: 30
+        }
+      }
+      // 创建运行速度数据
+      let extendYoumotong = {
+        'xAxis.0.axisLabel.rotate': 45,
+        legend: {
+          y: 'bottom',
+          padding: [10, 10],
+          selectedMode: false // 取消图例上的点击事件
+        },
+        title: {
+          show: true,
+          text: rawData.youmotong_title + rawData.unit,
+          // subtext: '副标题',
+          padding: [16, 8, 8, 8],
+          left: 10,
+          textStyle: {
+            fontWeight: 'normal',
+            fontSize: 16
+          }
+        },
+        grid: {
+          bottom: 30
+        }
+      }
+      // 创建表格属性集合
+      let extendArr = [extendPenfu, extendWuhua, extendYoumotong]
+      let chartTime = this.createChart(rawData.option.penfu)
+      let chartNumber = this.createChart(rawData.option.wuhua)
+      let chartSpeed = this.createChart(rawData.option.youmotong)
+      let chartArr = [chartTime, chartNumber, chartSpeed]
+      return {
+        extendArr,
+        chartArr
+      }
+    },
+    createChart (data) {
+      let columns = ['data']
+      // 处理legend
+      data.yAxis.forEach((value, key) => {
+        columns.push(value.name)
+      })
+      // 处理数据
+      let rows = []
+      // 获取每条x轴的长度
+      let colLen = columns.length
+      let xLen = data.xAxis.length
+      for (let i = 0; i < xLen; i++) {
+        let xData = data.xAxis[i]
+        let tmp = {}
+        for (let j = 0; j < colLen; j++) {
+          if (j === 0) {
+            tmp[columns[j]] = xData
+          } else {
+            tmp[columns[j]] = data.yAxis[j - 1].data[i]
+          }
+        }
+        rows.push(tmp)
+      }
+      return {
+        columns,
+        rows
+      }
     }
   },
-  methods: {
-  },
-  mounted () {
+  created () {
+    this._getMonitorList()
   }
 }
 </script>
 
-<style scoped lang="stylus">
+<style scoped lang='stylus'>
   .contianer
     display flex
     flex-direction column
